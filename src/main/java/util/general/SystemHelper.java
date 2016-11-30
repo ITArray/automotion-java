530d64e37e8a80f6146bf56e528ebe0776e1d8fa
Denys Zaiats <denys.zaiats@dreamit.de>
2016-11-30 08:54:52 +0100

    [master] - catching exceptions during validation for Retina display

--- src/main/java/util/general/SystemHelper.java
+++ src/main/java/util/general/SystemHelper.java
@@ -6,18 +6,22 @@ import java.lang.reflect.Field;
 public class SystemHelper {
     public static boolean isRetinaDisplay(Graphics2D g) {
         boolean isRetina = false;
-        GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-
         try {
-            Field field = graphicsDevice.getClass().getDeclaredField("scale");
-            if (field != null) {
-                field.setAccessible(true);
-                Object scale = field.get(graphicsDevice);
-                if (scale instanceof Integer && ((Integer) scale).intValue() == 2) {
-                    isRetina = true;
+            GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+            try {
+                Field field = graphicsDevice.getClass().getDeclaredField("scale");
+                if (field != null) {
+                    field.setAccessible(true);
+                    Object scale = field.get(graphicsDevice);
+                    if (scale instanceof Integer && ((Integer) scale).intValue() == 2) {
+                        isRetina = true;
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
+        }catch (Exception e){
             e.printStackTrace();
         }
         return isRetina;
