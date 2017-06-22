90615742f22742015e6090d7fedebefc9c2c8756
Torsten Mumme <torsten.mumme@dreamit.de>
2017-06-22 10:15:06 +0200

    deprecated packages environment and http.connections

---
+++ src/main/java/environment/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @deprecated As of release 2.0, replaced by{@link net.itarray.automotion.tools.environment}
+ */
+@Deprecated()
+package environment;
---
+++ src/main/java/http/connections/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @deprecated As of release 2.0, replaced by {@link net.itarray.automotion.tools.http.connections}
+ */
+@Deprecated()
+package http.connections;
