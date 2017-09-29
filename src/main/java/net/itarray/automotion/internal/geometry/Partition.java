13a7747636493e1e3932ade150fa1603256d5cd5
Torsten Mumme <torsten.mumme@dreamit.de>
2017-09-29 23:21:52 +0200

    simple partitioning

---
+++ src/main/java/net/itarray/automotion/internal/geometry/Partition.java
@@ -0,0 +1,46 @@
+package net.itarray.automotion.internal.geometry;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class Partition <T> {
+
+    private final List<List<T>> lists;
+
+    public Partition(List<T> objects, Function<T, Scalar> property) {
+        lists = new ArrayList<>();
+
+        List<T> sorted = new ArrayList<>(objects);
+        sorted.sort(Comparator.comparing(property));
+
+        int j = 0;
+        while (j < sorted.size()) {
+            List<T> currentPart = Lists.newArrayList();
+            lists.add(currentPart);
+            do {
+                currentPart.add(sorted.get(j));
+                j++;
+            } while (j < sorted.size() && continues(property, sorted.get(j), sorted.get(j-1)));
+        }
+    }
+
+    public boolean continues(Function<T, Scalar> property, T candidate, T last) {
+        Scalar lastValue = property.apply(last);
+        Scalar candidateValue = property.apply(candidate);
+        return candidateValue.minus(lastValue).isLessOrEqualTo(new Scalar(1));
+    }
+
+    public List<List<T>> getPartitions() {
+        return lists;
+    }
+
+
+    private <T> List<T> asList(T... objects) {
+        return Lists.newArrayList(objects);
+    }
+
+}
---
+++ src/test/java/net/itarray/automotion/tests/grid/PartitionTest.java
@@ -0,0 +1,67 @@
+package net.itarray.automotion.tests.grid;
+
+import net.itarray.automotion.internal.geometry.Partition;
+import net.itarray.automotion.internal.geometry.Scalar;
+import org.apache.commons.math3.fraction.Fraction;
+import org.apache.commons.math3.fraction.FractionFormat;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PartitionTest {
+
+    @Test
+    public void emptyListsProduceEmptyPartitions() {
+        assertPartitions(
+                asList());
+    }
+
+    @Test
+    public void objectsWithDistancesLessOrEqualToOneGoIntoTheSamePartition() {
+        assertPartitions(
+                asList("1", "2", "20", "21", "22"),
+                asList("1", "2"), asList("20", "21", "22"));
+    }
+
+    @Test
+    public void singleObjectAtEnd() {
+        assertPartitions(
+                asList("1", "2", "20", "21", "22", "25"),
+                asList("1", "2"), asList("20", "21", "22"), asList("25"));
+    }
+
+    @Test
+    public void objectsAreOrderedAccordingToThereValues() {
+        assertPartitions(
+                asList("20", "2", "1"),
+                asList("1", "2"), asList("20"));
+    }
+
+    private void assertPartitions(List<Object> objects, List<Object>... parts) {
+        assertPartitions(objects, Arrays.asList(parts));
+
+    }
+
+    private void assertPartitions(List<Object> objects, List<List<Object>> expected) {
+        Function<Object, Scalar> property = element -> ((Scalar) element);
+        Partition<Object> partition = new Partition<>(objects, property);
+        List<List<Object>> partitions = partition.getPartitions();
+        assertThat(partitions).isEqualTo(expected);
+    }
+
+
+    private List<Object> asList(String... objects) {
+        FractionFormat format = new FractionFormat();
+        return Arrays.stream(objects).map(s->(Object)new Scalar(format.parse(s))).collect(Collectors.toList());
+    }
+
+    private Fraction fraction(String literal) {
+        FractionFormat format = new FractionFormat();
+        return format.parse(literal);
+    }
+}
