118f57f3eafdbd61981bc36a0d3835e301812051
Denys Zaiats <denys.zaiats@dreamit.de>
2018-01-16 16:32:48 +0100

    [master] - fix for the improving of the screenshots

--- src/main/java/net/itarray/automotion/internal/HtmlReportBuilder.java
+++ src/main/java/net/itarray/automotion/internal/HtmlReportBuilder.java
@@ -79,6 +79,7 @@ public class HtmlReportBuilder {
                 new NoTag(this, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
                 new NoTag(this, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no\">");
 
+                new NoTag(this, "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>");
                 new NoTag(this, "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
                 new NoTag(this, "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">");
                 new NoTag(this, "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>");
@@ -209,13 +210,11 @@ public class HtmlReportBuilder {
                             new ClassAttribute("row")) {{
                         new Div(this,
                                 new ClassAttribute("col-xs-12 col-sm-6 col-md-4"),
-                                new Style("margin-top: 5px"),
                                 new Id("plot")) {{
                         }};
 
                         new Div(this,
-                                new ClassAttribute("col-xs-12 col-md-12"),
-                                new Style("margin-top: 5px"),
+                                new ClassAttribute("col-xs-12 col-md-8"),
                                 new Id("bar")) {{
                         }};
                     }};
@@ -362,7 +361,7 @@ public class HtmlReportBuilder {
                                 "\n" +
                                 "var layout = {\n" +
                                 "  height: 400,\n" +
-                                "  width: 500\n" +
+                                "  width: 500,\n" +
                                 "};\n" +
                                 "\n" +
                                 "Plotly.newPlot('plot', data, layout);");
@@ -376,8 +375,12 @@ public class HtmlReportBuilder {
                                 "    type: 'bar'\n" +
                                 "  }\n" +
                                 "];\n" +
+                                "var layout = {\n" +
+                                "  height: 400\n" +
+                                "};\n" +
                                 "\n" +
-                                "Plotly.newPlot('bar', data);");
+                                "\n" +
+                                "Plotly.newPlot('bar', data, layout);");
                     }};
 
                     jsonFiles.clear();
